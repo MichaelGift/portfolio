721fe2af81ac6c6b9effd2c7d9ceb3a53ad8e3da
Michael Gift <120593172+MichaelGift@users.noreply.github.com>
2024-01-24 21:15:34 +0000

    Update about me section

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Michael Gift",
+  title: "Mobile Developer & Game Developer",
+  email: "simiyugift@outlook.com",
+  gitHub: "MichaelGift",
+  instagram: "",
+  linkedIn: "smichaelgift",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "408Magnus",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
